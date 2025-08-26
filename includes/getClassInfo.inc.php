9df3e83af67adb5f88c16c6dd94c51efb094a4cc
Jayden Duncan <jaydendncn2019@gmail.com>
2025-08-25 23:25:37 -0500

    Design class info page

--- includes/getClassInfo.inc.php
+++ includes/getClassInfo.inc.php
@@ -8,11 +8,63 @@ require_once '../classes/studentclass.php';
 session_start();
 
 if(isset($_GET["info"])){
-    $id = $_GET["classid"];
+    $id = intval($_GET["classid"]);
 
-    echo "<script>alert('Getting student class info with id " . $id . "...');</script>";
+    $stuClassRow = findClassById($conn, $id);
+    $courseRow = findCourseById($conn, $stuClassRow["classesCourseId"]);
+
+    $courseId = $courseRow["coursesId"];
+    $courseAbbr = $courseRow["coursesAbbr"];
+    $courseNum = $courseRow["coursesCourseNum"];
+    $courseTitle = $courseRow["coursesTitle"];
+    $courseDesc = $courseRow["coursesDesc"];
+    $courseCreditHrs = $courseRow["coursesCreditHrs"];
+    $course = new Course($courseId, $courseAbbr, $courseNum, $courseTitle, $courseDesc, 
+                $courseCreditHrs);
+    $stuClass = new StudentClass($stuClassRow["classesId"], $course, $stuClassRow["classesSection"],
+                            $stuClassRow["classesInstructor"], $stuClassRow["classesStartTime"], 
+                            $stuClassRow["classesEndTime"], $stuClassRow["classesActiveDays"], $stuClassRow["classesType"]);
 }
 else{
     header("location: ../profile/planner.php");
     exit();
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title> Class Info Page </title>
+        <link rel="stylesheet" type="text/css" href="../css/getClassInfo.css">
+    </head>
+    <body>
+        <div class="classInfoDiv">
+            <h4 class="courseNum"><?php echo $stuClass->getCourse()->getAbbreviation() . " " . $stuClass->getCourse()->getCourseNumber()?></h4>
+            <p class="courseTitle"><b>Title:</b> <?php echo $stuClass->getCourse()->getTitle()?></p>
+            <p class="classSection"><b>Section:</b> <?php echo $stuClass->getSection()?></p>
+            <p class="courseCredits"><b>Credit Hours:</b> <?php echo $stuClass->getCourse()->getCreditHours()?></p>
+            <p class="classInstr"><b>Instructor:</b> <?php echo $stuClass->getInstructor()?></p>
+            <p class="classActiveDays"><b>Days:</b> <?php echo $stuClass->getActiveDays()?></p>
+            <p class="classTime">
+                <b>Time:</b>
+                <?php 
+                    $dateTimeImmutable = new DateTimeImmutable();
+
+                    if(!($stuClass->getType()==="ONL")) {
+                        $stFormatted = $dateTimeImmutable->setTimestamp($stuClass->getStartTime())->format('h:i A');
+                        $etFormatted = $dateTimeImmutable->setTimestamp($stuClass->getEndTime())->format('h:i A');
+
+                        echo $stFormatted . " - " . $etFormatted;
+                    }
+                    else{
+                        echo "N/A";
+                    }
+                ?>
+            </p>
+            <p class="classType"><b>Type:</b> <?php echo $stuClass->getType()?></p>
+            <p class="courseDesc"><b>Description:</b> <?php echo ($stuClass->getCourse()->getDescription()) ? $stuClass->getCourse()->getDescription() : "N/A"; ?></p>
+            <a href="../profile/planner.php"><button class="backBtn">Back</button></a>
+        </div>   
+    </body>
+</html>
