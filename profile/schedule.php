f5d8fc057f7a0fb4b576d72bda3fb0426a2fa716
Jayden Duncan <jaydendncn2019@gmail.com>
2025-09-20 22:58:27 -0500

    Schedule display scrollable grid

--- profile/schedule.php
+++ profile/schedule.php
@@ -5,12 +5,57 @@
                 <div class="info">
                     <div class="daysOfWeekHeader">
                         <p></p>
-                        <p></p>
                         <p>MON</p>
                         <p>TUE</p>
                         <p>WED</p>
                         <p>THU</p>
                         <p>FRI</p>
+                    </div>
+                    <div class="scheduleDiv">
+                        <div class="timeLabels">
+                            <p>12:00 AM</p>
+                            <p>1:00 AM</p>
+                            <p>2:00 AM</p>
+                            <p>3:00 AM</p>
+                            <p>4:00 AM</p>
+                            <p>5:00 AM</p>
+                            <p>6:00 AM</p>
+                            <p>7:00 AM</p>
+                            <p>8:00 AM</p>
+                            <p>9:00 AM</p>
+                            <p>10:00 AM</p>
+                            <p>11:00 AM</p>
+                            <p>12:00 PM</p>
+                            <p>1:00 PM</p>
+                            <p>2:00 PM</p>
+                            <p>3:00 PM</p>
+                            <p>4:00 PM</p>
+                            <p>5:00 PM</p>
+                            <p>6:00 PM</p>
+                            <p>7:00 PM</p>
+                            <p>8:00 PM</p>
+                            <p>9:00 PM</p>
+                            <p>10:00 PM</p>
+                            <p>11:00 PM</p>
+                        </div>
+                        <div class="monClasses">
+                            <div id="CS101Slot" class="classSlot">
+                                <h2>CS 101</h2>
+                                <p>12:00AM - 1:00AM</p>
+                            </div>
+                        </div>
+                        <div class="tueClasses">
+
+                        </div>
+                        <div class="wedClasses">
+                            
+                        </div>
+                        <div class="thuClasses">
+                            
+                        </div>
+                        <div class="friClasses">
+                            
+                        </div>
                     </div>
                 </div>
 
